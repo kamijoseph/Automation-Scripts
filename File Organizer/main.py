893b7d3d4e58611fca8417f4be83c055167fb33e
Kami <josephkiarie561@gmail.com>
2025-12-27 19:33:38 +0300

    logging configuration

--- File Organizer/main.py
+++ File Organizer/main.py
@@ -1,1 +1,22 @@
+
 # file organiser
+
+import os
+import shutil
+import logging
+from pathlib import Path
+from datetime import datetime
+
+#  logging
+LOG_DIR = Path.home() / ".file_organizer_logs"
+LOG_DIR.mkdir(exist_ok=True)
+LOG_FILE = LOG_DIR / f"organizer_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = "%(asctime)s | %(levelname)s | %(message)s",
+    handlers = [
+        logging.FileHandler(LOG_FILE),
+        logging.StreamHandler()
+    ]
+)
